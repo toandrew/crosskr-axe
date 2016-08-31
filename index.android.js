211146e35d45d8a67393cb76cb65f5f1418a28c4
Jianmin Zhou <zhoujianmin@xiaoyezi.com>
2016-08-31 11:06:06 +0800

    Change name to CrossKrAxe.

---
+++ index.android.js
@@ -0,0 +1,14 @@
+/**
+ * Sample React Native App s
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry
+} from 'react-native';
+
+import CrossKrAxe from './app/container/root';
+
+AppRegistry.registerComponent('CrossKrAxe', () => CrossKrAxe);
