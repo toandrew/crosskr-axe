86fce192e2f2d21ecd06466fe2dd47e77f47b5f3
Jianmin Zhou <zhoujianmin@xiaoyezi.com>
2016-09-08 20:00:01 +0800

    Support home navi.

---
+++ app/pages/home.js
@@ -0,0 +1,32 @@
+'use strict';
+
+import React, {Component} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet
+} from 'react-native';
+
+class Home extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  render() {
+    <View>
+      <Text>
+        This is home page!
+      </Text>
+    </View>
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default Home;
--- index.android.js
+++ index.android.js
@@ -1,5 +1,5 @@
 /**
- * Sample React Native App s
+ * Sample React Native App
  * https://github.com/facebook/react-native
  * @flow
  */
